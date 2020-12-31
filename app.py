fdd37bd9450a2fbe677db208de27d69671ea5aa2
proprgrmmr <61632722+sam0132nodier@users.noreply.github.com>
2020-12-31 07:41:15 +0300

    Add navigation buttons

--- app.py
+++ app.py
@@ -122,7 +122,17 @@ class Window(Frame):
 			widgets.destroy()
 
 		# Create the title of the frame for user orientation
-		Label(self.frame, text='Click the COPY button\nTo copy the password into the clipboard', font=('Arial', 12)).grid(row=0, column=0, columnspan=3,  pady=20)
+		Label(self.frame, text='Click the COPY button\nTo copy the password into the clipboard', font=('Arial', 12)).grid(row=0, column=0, columnspan=4,  pady=20)
+
+		# Create two navigation buttons
+		self.add_btn = Button(self.frame, text='Add', font=('Arial', 10), width=12, height=1, relief=SOLID, borderwidth=1)
+		self.add_btn.grid(row=1, column=0, ipady=2, pady=(10,10), padx=(0, 20))
+		self.add_btn.bind('<Button-1>', self.add_password)
+
+		self.view_btn = Button(self.frame, text='View', font=('Arial', 10), width=12, height=1, relief=SOLID, borderwidth=1)
+		self.view_btn.grid(row=1, column=1, ipady=2, pady=(10,10), padx=(20, 0))
+		self.view_btn.bind('<Button-1>', self.view_passwords)
+
 
 		# Create the connection to the database
 		connection = sqlite3.connect(DATABASE)
